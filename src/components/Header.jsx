30d8007af6d4f96195e1b891208ed7dcf8fc2d11
TabhitaBlackmore <gergana.k.ivanova.work@abv.bg>
2024-02-06 21:37:47 +0200

    Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,14 @@
 export default function Header() {
-    return(
-        <h2>Hi I am the Header</h2>
-    )
+  return (
+    <header className="md:flex lg:flex md:bg-dark lg:bg-dark">
+      <div className="flex bg-dark h-13.5 md:h-20 pl-6 pt-2 md:pl-7.5 md:pt-3">
+        <img className="w-10.5 h-10.5 md:w-13 md:h-13 rounded-full" src="logo.jpg" alt="logo" />
+        <h4 className="font-sans text-white text-sm md:text-xl lg:text-2xl pl-2 pt-3 md:pl-3 md:pt-4 lg:pl-3 lg:pt-3.5">Logo</h4>
+      </div>
+      <div className="sm:bg-gray md:bg-dark lg:bg-dark pt-2 pb-2 md:pr-28 lg:pr-28 m-auto text-center">
+        <h1 className="text-xl md:text-4xl text-white text-shadow-glow">LOREM IPSUM</h1>
+        <p className="text-darkwhite sm:text-xxs md:text-sm lg:text-xl">LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM</p>
+      </div>
+    </header>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,53 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '200px',
+      'md': '768px',
+      'lg': '1280px',
+    },
+    colors: {
+      gray: '#1a1a21',
+      dark: '#121212',
+      white: '#FFFFFF',
+      darkwhite: '#929292',
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      spacing: {
+        '7.5': '1.875rem', //30px
+        '10.5': '2.625rem', //42px
+        '13': '3.4375rem', //55px
+        '13.5': '3.75rem', //60px
+      },
+      textShadow: {
+        glow: '1px 0 10px #05FF00',
+      },
+      fontSize: {
+        'xxs': '0.625rem', //10px
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
 
